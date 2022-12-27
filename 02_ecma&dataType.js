65f54d517e92919a99a31705bd66ef0a4a9493e4
aniketjha9179 <aniketjha9179@gmail.com>
2022-12-27 22:44:13 +0530

    javascript basics

---
+++ 02_ecma&dataType.js
@@ -0,0 +1,29 @@
+// new varsion javascript of 2019, 2022 or newer   
+"use strict";//treat all Javascript code as newer version
+// ("use strict";) if we are using this line then whole code will  written in  newer version of js engine
+// now days js engine already uses the strict mode by default
+
+// alert(2+2) WILLNOT WORK AS WE ARE USING NODEJS, NOT BROWSER
+
+let name="Aniket Jha"//string dataType
+let age=20;//int datatype (range 2 to power 53-1)
+// big interger (used in  stock market, vast websites such as reddit etc)
+let isLoggedIn=true//booleanType is  yes or no just (0 or 1)
+
+// null  (stand alone value) intentionally 
+// we can assign the value as null 
+//if we are working on something like tempreture system and we assign value as 0 then it will consider as tempreture so instead of 0 we will use null that it .
+
+// undefined
+
+// Symbol => for uniquesness   we use symbol 
+
+
+//object  
+console.log(typeof"AJ");
+console.log(typeof age);
+console.log(typeof undefined);//undefined
+
+
+console.log(typeof null);//null is an object 
+
