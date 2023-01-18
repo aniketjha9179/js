b7d12fd8a0b525d356eabc284e3d2f23ee266f66
aniketjha9179 <aniketjha9179@gmail.com>
2023-01-18 18:15:46 +0530

    M

---
+++ HOF.js
@@ -0,0 +1,24 @@
+function salary(num){
+    return num * .7;
+
+}
+
+setTimeout(salary,300)
+
+const mySalary =[300,50,599,434,43,];
+
+mySalary.filter(salary).filter(tax).map();
+
+()=>{}
+
+(func)=>{}
+
+(func)=>async()=>{}
+
+const asyncHandler = (fn)=>async(req, res,next){
+    try{await fn(req,res,next)
+
+    }catch(error){
+        
+    }
+}
