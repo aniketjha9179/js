2cee2c11da2dde82531c16a3978a65d0cc39ca1d
aniketjha9179 <aniketjha9179@gmail.com>
2023-01-18 16:42:01 +0530

    null

---
+++ operation.js
@@ -0,0 +1,34 @@
+
+// let value =3;
+// let nevValue =-value;
+// console.log(nevValue);
+
+// new 
+// console.log("1" + "2");
+// console.log(2+1+"2");
+// console.log('3'+ 2+2  );
+// console.log(+"");
+// console.log(true+);
+
+
+// let gameCounter= 100;
+// gameCounter++;
+// console.log(gameCounter);
+
+
+// these conversion are confusing 
+console.log(null>0);
+console.log(null == 0);
+// in comparision operator  javascript converts
+//   the null into  number this is the reason it is showing true  
+console.log(null>=0);
+
+
+
+console.log(undefined == 0) //this work different === strictly check data type also
+console.log(undefined >= 0);
+console.log(undefined > 0);
+//this work different === strictly 
+console.log("2"===2);
+// this will give true  as it does not check  dataype
+console.log("2"==2);
