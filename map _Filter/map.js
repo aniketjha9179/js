ada0aaba2e7fb91682425dd4438e7e50bbf33326
aniketjha9179 <aniketjha9179@gmail.com>
2023-04-02 16:40:22 +0530

    map

--- map _Filter/map.js
+++ map _Filter/map.js
@@ -1,21 +1,21 @@
-// // map creates a new array
-// // map take 3 argue, index , value , array
-// // ARRAY MAP METHOD
-// let arr = [45, 23, 21];
-// let a = arr.map((value, index, array) => {
-//   //   console.log(value, index, array);
-//   //   return value + value;
-// });
-// // console.log(a, arr);
-// // foreach   is a loop and goes through every array element
+// map creates a new array
+// map take 3 argue, index , value , array
+// ARRAY MAP METHOD
+let arr = [45, 23, 21];
+let a = arr.map((value, index, array) => {
+  //   console.log(value, index, array);
+  //   return value + value;
+});
+// console.log(a, arr);
+// foreach   is a loop and goes through every array element
 
-// //ARRAY FILTER METHOD
+//ARRAY FILTER METHOD
 
-// let arr2 = [51, 31, 11, 2, 5, 6, 8];
-// let a2 = arr2.filter((a) => {
-//   return a < 10;
-// });
-// console.log(a2, arr2);
+let arr2 = [51, 31, 11, 2, 5, 6, 8];
+let a2 = arr2.filter((a) => {
+  return a < 10;
+});
+console.log(a2, arr2);
 
 //REDUCE METHOD
 
