717225d80748d85e03afa860421f3620b41c345d
aniketjha9179 <aniketjha9179@gmail.com>
2023-06-06 12:35:58 +0530

    conrols stat... truthy, falsy, NullishCoalcing(??)

---
+++ 04_control-flow/switch.js
@@ -0,0 +1,30 @@
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 'April';
+switch (month) {
+    case 'jan':
+        console.log('January');
+        break;
+    case 'fab':
+        console.log('fabuary');
+        break;
+    case 'mar':
+        console.log('March');
+        break;
+    case 'April':
+        console.log('April');
+        break;
+
+    default:
+        console.log('default case match');
+        break;
+}
